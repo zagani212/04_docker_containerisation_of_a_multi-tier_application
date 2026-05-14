6da20ae16d8181f0e667d4c75403cb4d939b8508
zagani212 <abdelhakassahi@gmail.com>
2026-05-14 22:07:17 +0200

    create private docker regitry

---
+++ gcp/main.tf
@@ -0,0 +1,23 @@
+provider "google" {
+  project     = "project-55dcf55b-027c-4122-a45"
+  region      = "us-central1"
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "terraform_state_bucket_abdelhak"
+    prefix = "terraform/state"
+  }
+}
+
+resource "google_artifact_registry_repository" "my-repo" {
+  location      = "us-central1"
+  repository_id = "my-repository"
+  description   = "docker repository"
+  format        = "DOCKER"
+
+  docker_config {
+    immutable_tags = true
+  }
+}
+
