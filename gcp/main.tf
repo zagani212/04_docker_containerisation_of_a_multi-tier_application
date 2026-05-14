83adce862b33da5e32fc408cddb1d1ba956b8e73
zagani212 <abdelhakassahi@gmail.com>
2026-05-14 22:40:13 +0200

    cerate a vm and attach a SA

--- gcp/main.tf
+++ gcp/main.tf
@@ -21,3 +21,36 @@ resource "google_artifact_registry_repository" "my-repo" {
   }
 }
 
+
+resource "google_service_account" "vm_sa" {
+  account_id   = "my-vm-service-account"
+  display_name = "Custom SA for VM Instance"
+}
+
+resource "google_project_iam_member" "storage_access" {
+  project = "project-55dcf55b-027c-4122-a45"
+  role    = "roles/registry.writer"
+  member  = "serviceAccount:${google_service_account.vm_sa.email}"
+}
+
+resource "google_compute_instance" "app_server" {
+  name         = "app-server"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  service_account {
+    email  = google_service_account.vm_sa.email
+    scopes = ["cloud-platform"]
+  }
+}
