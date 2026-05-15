e3342603c24aed17c57af495238ee495b01a65a2
zagani212 <abdelhakassahi@gmail.com>
2026-05-15 20:04:42 +0200

    scan docker images before push

--- gcp/main.tf
+++ gcp/main.tf
@@ -20,37 +20,3 @@ resource "google_artifact_registry_repository" "my-repo" {
     immutable_tags = true
   }
 }
-
-
-resource "google_service_account" "vm_sa" {
-  account_id   = "my-vm-service-account"
-  display_name = "Custom SA for VM Instance"
-}
-
-resource "google_project_iam_member" "storage_access" {
-  project = "project-55dcf55b-027c-4122-a45"
-  role    = "roles/artifactregistry.writer"
-  member  = "serviceAccount:${google_service_account.vm_sa.email}"
-}
-
-resource "google_compute_instance" "app_server" {
-  name         = "app-server"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    network = "default"
-    access_config {}
-  }
-
-  service_account {
-    email  = google_service_account.vm_sa.email
-    scopes = ["cloud-platform"]
-  }
-}
